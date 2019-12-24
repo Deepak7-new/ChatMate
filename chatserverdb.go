0c06f9be8737fda66afa3a9c58bd69afd09fa334
Deepak7-new <55599102+Deepak7-new@users.noreply.github.com>
2019-12-25 00:35:17 +0530

    Add files via upload

---
+++ chatserverdb.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"chat"
+	"context"
+	"log"
+	"net"
+
+	//"sync"
+	"time"
+
+	"./sqlconnect"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
+)
+
+//var mux sync.Mutex
+
+type server struct{}
+
+func main() {
+	//listener, err := net.Listen("tcp", ":8080")
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Println("error:", err)
+	}
+	log.Printf("Started")
+	srv := grpc.NewServer()
+	chat.RegisterChatsServer(srv, &server{})
+	reflection.Register(srv)
+
+	if e := srv.Serve(listener); e != nil {
+		log.Println("error:", e)
+	}
+}
+
+func (s *server) Join(ctx context.Context, request *chat.SendJoin) (*chat.GetJoin, error) {
+	uname := request.Name
+	//log.Printf(uname)
+	if uname == "" {
+		log.Printf("Name cannot be empty")
+		return nil, status.Errorf(codes.InvalidArgument,
+			"Name cannot be empty")
+	}
+	//mux.Lock()
+	sqlconnect.Userdao(uname)
+	//mux.Unlock()
+
+	return &chat.GetJoin{}, nil
+}
+
+func (s *server) Send(ctx context.Context, request *chat.SendMessage) (*chat.GetMessage, error) {
+	sender := request.From
+	receiver := request.To
+	if sender == "" || receiver == "" {
+		return nil, status.Errorf(codes.InvalidArgument,
+			"Name cannot be empty")
+	}
+	//mux.Lock()
+	uid := sqlconnect.Senddao(receiver, sender, request.Smsg)
+	//log.Printf("%d", uid)
+	//mux.Unlock()
+	return &chat.GetMessage{Id: int64(uid), From: sender, To: receiver, Rmsg: request.Smsg, Time: time.Now().String()}, nil
+}
+
+func (s *server) Message(ctx context.Context, request *chat.SendAllMessage) (*chat.GetAllMessage, error) {
+	ofName := request.Usr
+	grp := sqlconnect.Allmessages(ofName)
+
+	full := make([]*chat.GetMessage, 0)
+	for _, ele := range grp {
+		allmsg := &chat.GetMessage{}
+		allmsg.Id = ele.Id
+		allmsg.From = ele.From
+		allmsg.To = ele.To
+		allmsg.Rmsg = ele.Message
+		allmsg.Time = ele.Time.String()
+		full = append(full, allmsg)
+	}
+
+	return &chat.GetAllMessage{Msgs: full}, nil
+}
+
+func (s *server) Users(ctx context.Context, request *chat.SendUsers) (*chat.GetUsers, error) {
+	log.Printf("users")
+	//mux.Lock()
+	keys := sqlconnect.Allusers()
+	//mux.Unlock()
+	log.Printf("Users: %v", keys)
+	return &chat.GetUsers{Names: keys}, nil
+}
